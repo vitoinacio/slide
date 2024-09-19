fbd2e69307f9480718dab6da83fe1f99d6c0f70c
Victor Hugo <victor.hugo.ina10@gmail.com>
2024-09-19 12:43:22 -0300

    custon controls estilizados

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
-import { SlideNav } from "./modules/slide.js";
+import SlideNav from "./modules/slide.js";
 
 const slide = new SlideNav(".slide", ".slide-wrapper");
 slide.init();
 slide.addArrow(".prev", ".next");
-slide.addControl();
+slide.addControl('.custon-controls');
